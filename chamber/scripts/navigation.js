56a7ba3d6cbe40713235e499406585bde3b936fc
Itoro Etim Akpan Mendie <itoroakpan@email.com>
2026-05-18 17:20:08 +0100

    week 2 assignment fix

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,15 @@
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector("#navigation");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+
+    const expanded =
+        menuButton.getAttribute("aria-expanded") === "true";
+
+    menuButton.setAttribute(
+        "aria-expanded",
+        !expanded
+    );
+});
